12debcfe2ac7d267cead31d29054e937852221cc
Rahul Shahaney <rahuls@thewhiteboard.in>
2019-06-22 14:32:44 -0700

    Changes to code flow

--- engg_challenge.py
+++ engg_challenge.py
@@ -12,7 +12,6 @@ def caller(smaller_file, memory, size, file_2):
     memory = int(memory)*(10**6)
     chunk = float(size)
     file = open(smaller_file)
-    print(size)
     #deciding chunk size as a function of available memory
     while chunk > (memory/4):
         chunk = chunk/2
@@ -20,12 +19,11 @@ def caller(smaller_file, memory, size, file_2):
     while True:
         hash_set = set()
         next_n_lines = list(islice(file, number_of_lines))
-        if not next_n_lines:
+        if len(next_n_lines) == 0:
             break
         for element in next_n_lines:
             hash_set.add(int(element.strip()))
         counter+=match_checker(hash_set, file_2)
-
 #    for line in open(smaller_file):
 #       line = line.strip()
 #       hash_set.add(int(line))
@@ -45,14 +43,12 @@ def determine_size(input1, input2, available_memory):
     size_of_first = os.stat(input1).st_size
     size_of_second = os.stat(input2).st_size
     if size_of_first > size_of_second:
-        hash_set = caller(input1,available_memory,size_of_first,input2)
-        number_of_matches = match_checker(hash_set, input2)
+        number_of_matches = caller(input1,available_memory,size_of_first,input2)
     else:
-        hash_set = caller(input2,available_memory,size_of_second,input1)
-        number_of_matches = match_checker(hash_set, input1)
+        number_of_matches = caller(input2,available_memory,size_of_second,input1)
     return number_of_matches
 
 number = determine_size(sys.argv[1],sys.argv[2], sys.argv[3])
 
-#print(number)
+print(number)
 
