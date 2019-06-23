ee39a9ffa7000416b9910c5dd4db816554d5fc8b
Rahul Shahaney <rahuls@thewhiteboard.in>
2019-06-22 17:20:51 -0700

    engineering_challege

--- engg_challenge.py
+++ engg_challenge.py
@@ -5,7 +5,7 @@ import random
 import tempfile
 import math
 from itertools import islice
-
+from memprof import memprof
 
 def caller(smaller_file, memory, size, file_2):
     counter = 0
@@ -13,7 +13,7 @@ def caller(smaller_file, memory, size, file_2):
     chunk = float(size)
     file = open(smaller_file)
     #deciding chunk size as a function of available memory
-    while chunk > (memory/4):
+    while chunk > (memory/3):
         chunk = chunk/2
     number_of_lines = math.ceil(chunk/18)
     while True:
@@ -23,10 +23,7 @@ def caller(smaller_file, memory, size, file_2):
             break
         for element in next_n_lines:
             hash_set.add(int(element.strip()))
-        counter+=match_checker(hash_set, file_2)
-#    for line in open(smaller_file):
-#       line = line.strip()
-#       hash_set.add(int(line))
+        counter += match_checker(hash_set, file_2)
     return counter
 
 
@@ -38,17 +35,16 @@ def match_checker(hash_set, file_in_disk):
     return counter
 
 
-
 def determine_size(input1, input2, available_memory):
     size_of_first = os.stat(input1).st_size
     size_of_second = os.stat(input2).st_size
     if size_of_first > size_of_second:
-        number_of_matches = caller(input1,available_memory,size_of_first,input2)
+        number_of_matches = caller(input2,available_memory,size_of_secod,input1)
     else:
-        number_of_matches = caller(input2,available_memory,size_of_second,input1)
+        number_of_matches = caller(input1,available_memory,size_of_first,input2)
     return number_of_matches
 
-number = determine_size(sys.argv[1],sys.argv[2], sys.argv[3])
 
+number = determine_size(sys.argv[1],sys.argv[2], sys.argv[3])
 print(number)
 
